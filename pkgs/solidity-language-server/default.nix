2afbb297183dc456481f816b5ca46cbe0c367356
Coosis <1159727122@qq.com>
2024-12-24 00:01:43 +0800

    solidity lsp

---
+++ pkgs/solidity-language-server/default.nix
@@ -0,0 +1,33 @@
+{ stdenv
+, lib
+, fetchurl
+, nodejs
+}:
+stdenv.mkDerivation {
+  pname = "solidity-language-server";
+  version = "0.8.7";
+
+	nativeBuildInputs = [ nodejs ];
+
+  src = fetchurl {
+		url = "https://registry.npmjs.org/@nomicfoundation/coc-solidity/-/coc-solidity-0.8.7.tgz";
+		sha256 = "";
+  };
+
+	buildPhase = ''
+		npm install
+	'';
+
+  installPhase = ''
+		mkdir -p $out/bin
+		ln -s $src/node_modules/.bin/nomicfoundation-solidity-language-server $out/bin/solidity-language-server
+	'';
+
+  meta = {
+    description = "Language server for Solidity";
+    homepage = "https://github.com/NomicFoundation/hardhat-vscode";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}
+
